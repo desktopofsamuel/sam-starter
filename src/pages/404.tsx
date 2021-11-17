e22a47a343d3a164324cb594c4a54bb009ff7754
Samuel Wong <35249407+desktopofsamuel@users.noreply.github.com>
2021-11-17 23:17:57 +0800

    fix: build error

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Text } from '@chakra-ui/react';
 import { PageProps } from 'gatsby';
+import Layout from '@/components/layout';
 
 const NotFound: React.FC<PageProps> = () => (
   <Layout>
